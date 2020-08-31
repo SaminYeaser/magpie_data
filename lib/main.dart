8716dba969532a977caa8047c703d5ab04287499
Samin <saminyeaser1@gmail.com>
2020-08-31 18:09:56 +0600

    running

--- lib/main.dart
+++ lib/main.dart
@@ -3,9 +3,7 @@ import 'view/showData.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
-void main() async {
-  List _data = await getData();
-
+void main() {
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
     title: 'MagpieData',
@@ -13,38 +11,7 @@ void main() async {
       primarySwatch: Colors.blue,
       visualDensity: VisualDensity.adaptivePlatformDensity,
     ),
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text('Magpie Data'),
-        centerTitle: true,
-        backgroundColor: Colors.indigo,
-      ),
-      body: Center(
-        child: ListView.builder(
-          itemCount: _data.length,
-          itemBuilder: (BuildContext context, int position) {
-            return Card(
-              child: ListTile(
-                title: Text("${_data[position]['date']}"),
-              ),
-            );
-          },
-        ),
-      ),
-    ),
+    home: AnimationData()
   ));
 }
 
-//class MyApp extends StatelessWidget {
-//  // This widget is the root of your application.
-//  @override
-//  Widget build(BuildContext context) {
-//    return
-//  }
-//}
-
-Future<List> getData() async {
-  String api = "https://magpiedata-15eaf.firebaseio.com/.json";
-  http.Response response = await http.get(api);
-  return jsonDecode(response.body);
-}
--- lib/view/showData.dart
+++ lib/view/showData.dart
@@ -1,18 +1,62 @@
-import 'dart:convert';
-
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class Home extends StatefulWidget {
+class AnimationData extends StatefulWidget {
   @override
-  _HomeState createState() => _HomeState();
+  _AnimationState createState() => _AnimationState();
 }
 
-class _HomeState extends State<Home> {
+class _AnimationState extends State<AnimationData> {
 
+  List datam = new List();
+  getDetails() async{
+    List data = await getData();
+    datam = data;
+    setState(() {
+
+    });
+  }
+  @override
+  void initState() {
+      getDetails();
+    super.initState();
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Magpie Data'),
+        centerTitle: true,
+        backgroundColor: Colors.indigo,
+      ),
+      body: Center(
+        child: ListView.builder(
+          itemCount: datam.length,
+          itemBuilder: (BuildContext context, int position) {
+            return Card(
+              child: ListTile(
+                contentPadding: EdgeInsets.symmetric(vertical: 10),
+                title: Text("${datam[position]['date']}"),
+                subtitle: Text("""
+                  close: ${datam[position]['close']}
+                  high: ${datam[position]['high']}
+                  low: ${datam[position]['low']}
+                  open: ${datam[position]['open']}
+                  trade_code: ${datam[position]['trade_code']}
+                  volume: ${datam[position]['volume']}
+                  
+                """),
+              ),
+            );
+          },
+        ),
+      ),
+    );
   }
 }
-
+Future<List> getData() async {
+  String api = "https://magpiedata-15eaf.firebaseio.com/.json";
+  http.Response response = await http.get(api);
+  return jsonDecode(response.body);
+}
